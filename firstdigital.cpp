d7f9eb2150d1062582a8f5a3b860cc6bbdca7d46
lightproger <photoluxe@mail.ru>
2017-12-06 01:18:55 +0600

    update

--- firstdigital.cpp
+++ firstdigital.cpp
@@ -6,7 +6,7 @@ int main() {
     int H = N / 3600 % 24;
     int M = N % 3600 / 60;
     int S = N % 3600 % 60;
-    cout << H << ":" << M << ":" << S << endl;
+    cout << H << ":" << M / 10 << M % 10 << ":" << S / 10 << S % 10 << endl;
 
     return 0;
 }
